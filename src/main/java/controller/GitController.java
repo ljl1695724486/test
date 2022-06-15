6427e7360562aa9ff91db018f833ff8c60be36a1
admin <admin@qq.com>
2022-06-15 15:03:59 +0800

    6666666666666666666666666666666

--- src/main/java/controller/GitController.java
+++ src/main/java/controller/GitController.java
@@ -2,6 +2,9 @@ package controller;
 
 public class GitController {
     public static void main(String[] args) {
+        System.out.println("hot fix666666666666666666666666666666666");
+    }
+    public static void main(String[] args) {
         System.out.println("111111111111111111111111");
         System.out.println("hot fix 1111111111111111111");
         System.out.println("2222222222222222222222222");
